d85c35d2c54f8916f4bf08f22479148e1fcfb5b3
YI <uuuuuu@protonmail.com>
2020-03-20 19:50:45 +0800

    add get balance for fee receiver

--- src/constants.rs
+++ src/constants.rs
@@ -3,14 +3,16 @@ use curve25519_dalek::constants::RISTRETTO_BASEPOINT_POINT;
 use curve25519_dalek::ristretto::RistrettoPoint;
 use rand_chacha::ChaCha20Rng;
 use rand_core::SeedableRng;
-use sha2::Sha512;
+use sha2::{Digest, Sha512, Sha512Trunc256};
 
 use crate::{PublicKey, SecretKey};
 
 pub(crate) static MERLIN_CONFIDENTIAL_TRANSACTION_LABEL: &[u8] =
     b"suter_confidential_transaction_proof";
 
-pub(crate) static RANDOM_RISTRETTO_POINT_HASH_INPUT: &[u8] = b"random_ristretto_point_hash_input";
+static RANDOM_RISTRETTO_POINT_HASH_INPUT: &[u8] = b"random_ristretto_point_hash_input";
+
+static RANDOM_SK_HASH_INPUT: &[u8] = b"random_sk_hash_input";
 
 pub const BASE_POINT: RistrettoPoint = RISTRETTO_BASEPOINT_POINT;
 
@@ -25,7 +27,10 @@ lazy_static! {
         RistrettoPoint::hash_from_bytes::<Sha512>(RANDOM_RISTRETTO_POINT_HASH_INPUT)
     );
     pub(crate) static ref FEE_KEYPAIR: (SecretKey, PublicKey) = {
-        let mut csprng: ChaCha20Rng = SeedableRng::seed_from_u64(32938408097);
+        let mut hasher = Sha512Trunc256::new();
+        hasher.input(RANDOM_SK_HASH_INPUT);
+        let result = hasher.result();
+        let mut csprng: ChaCha20Rng = SeedableRng::from_seed(result.into());
         let sk = SecretKey::generate_with(&mut csprng);
         let pk = sk.to_public();
         (sk, pk)
